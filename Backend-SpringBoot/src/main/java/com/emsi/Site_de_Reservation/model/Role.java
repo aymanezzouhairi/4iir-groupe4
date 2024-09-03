fb6f434d81fb3a8b76d6a97b301af5c4e189a70b
aymanezzouhairi <121771900+aymanezzouhairi@users.noreply.github.com>
2024-09-03 09:28:57 +0100

    Delete Backend-SpringBoot directory

--- Backend-SpringBoot/src/main/java/com/emsi/Site_de_Reservation/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.emsi.Site_de_Reservation.model;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- Backend-SpringBoot/src/main/java/com/emsi/Site_de_Reservation/repository/ActivityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.emsi.Site_de_Reservation.repository;
-
-import com.emsi.Site_de_Reservation.model.Activity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActivityRepository extends JpaRepository<Activity, Long> {
-
-}
